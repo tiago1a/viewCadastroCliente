cb4b9f520827d8bc44698719cbcadeefa349376b
Tiago Soares <tiago.soares@cooperforte.coop.br>
2019-11-25 15:04:30 -0300

    atualizacao cadastral

--- cadastra-cliente/src/App.js
+++ cadastra-cliente/src/App.js
@@ -16,6 +16,7 @@ class App extends Component {
     this.state = { listaCliente: [], id: '', cpf: '', nome: '', email: '', telefone: '', senha: '', usuario: '' };
 
     this.enviaForm = this.enviaForm.bind(this);
+    this.editaForm = this.editaForm.bind(this);
     this.setcpf = this.setcpf.bind(this);
     this.setNome = this.setNome.bind(this);
     this.setTelefone = this.setTelefone.bind(this);
@@ -56,7 +57,6 @@ class App extends Component {
     })
   }
   excluirForm(id) {
-    console.log(id);
     $.ajax({
       url: `http://localhost:8080/cliente/${id}`,
       headers: {
@@ -68,104 +68,125 @@ class App extends Component {
     })
   }
 
+  editaForm(id) {
+    //CARREGAR OS DADOS DO ID NA TELA
+    $.ajax({
+      url: `http://localhost:8080/cliente/${id}`,
+      dataType: 'json',
+      success: function (resposta) {
+        this.setState({ listaCliente: resposta });
+      }.bind(this)
+    });
+  
+    //EDITAR O CLIENTE
+//    $.ajax({
+//        url: `http://localhost:8080/cliente/${id}`,
+//        headers: {
+//          'Accept': 'application/json',
+//          'Content-Type': 'application/json',
+//          'charset': 'utf8'
+//        },
+//        type: 'delete'
+//      })
+}
+
 
 setcpf(evento) {
-  this.setState({ cpf: evento.target.value })
-}
+      this.setState({ cpf: evento.target.value })
+    }
 
 setNome(evento) {
-  this.setState({ nome: evento.target.value })
-}
+      this.setState({ nome: evento.target.value })
+    }
 setTelefone(evento) {
-  this.setState({ telefone: evento.target.value })
-}
+      this.setState({ telefone: evento.target.value })
+    }
 setEmail(evento) {
-  this.setState({ email: evento.target.value })
-}
+      this.setState({ email: evento.target.value })
+    }
 setUsuario(evento) {
-  this.setState({ usuario: evento.target.value })
-}
+      this.setState({ usuario: evento.target.value })
+    }
 setSenha(evento) {
-  this.setState({ senha: evento.target.value })
-}
+      this.setState({ senha: evento.target.value })
+    }
 
 render() {
-  console.log("render");
-  return (
-    <div id="layout">
-      <a href="#menu" id="menuLink" className="menu-link">
-        <span></span>
-      </a>
+      console.log("render");
+      return(
+    <div id = "layout" >
+          <a href="#menu" id="menuLink" className="menu-link">
+            <span></span>
+          </a>
 
-      <div id="menu">
-        <div className="pure-menu">
-          <a className="pure-menu-heading" href="#">Company</a>
-          <ul className="pure-menu-list">
-            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Cliente</a></li>
-            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Lista Cliente</a></li>
-          </ul>
-        </div>
-      </div>
-
-      <div id="main">
-        <div className="header">
-          <h1>CLIENTES</h1>
-        </div>
-        <div className="content" id="content">
-          <div className="pure-form pure-form-aligned">
-            <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
-              <InputCustomizado id="cpf" type="text" name="cpf" value={this.state.cpf} onChange={this.setcpf} label="CPF" />
-              <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome" />
-              <InputCustomizado id="telefone" type="text" name="telefone" value={this.state.telefone} onChange={this.setTelefone} label="Telefone" />
-              <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email" />
-              <InputCustomizado id="usuario" type="text" name="usuario" value={this.state.usuario} onChange={this.setUsuario} label="Usuário" />
-              <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha" />
-              <label></label>
-              <button type="submit" className="pure-button pure-button-primary">Gravar</button>
-            </form>
-
+          <div id="menu">
+            <div className="pure-menu">
+              <a className="pure-menu-heading" href="#">Company</a>
+              <ul className="pure-menu-list">
+                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Cliente</a></li>
+                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Lista Cliente</a></li>
+              </ul>
+            </div>
           </div>
-          <div>
-            <table className="pure-table">
-              <thead>
-                <tr>
-                  <th>CPF</th>
-                  <th>Nome</th>
-                  <th>Telefone</th>
-                  <th>Email</th>
-                  <th>Usuario</th>
-                  <th>Senha</th>
-                  <th>Ação</th>
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  this.state.listaCliente.map((cliente) => {
-                    
-                    return (
-                      <tr key={cliente.id}>
-                        <td>{cliente.cpf}</td>
-                        <td>{cliente.nome}</td>
-                        <td>{cliente.telefone}</td>
-                        <td>{cliente.email}</td>
-                        <td>{cliente.usuario}</td>
-                        <td>{cliente.senha}</td>
-                        <td>{cliente.acao}
-                          <button name='excluir' onClick={() => this.excluirForm(cliente.id)}><IoIosTrash /></button>
-                          
-                        </td>
-                      </tr>
-                    );
-                  })
-                }
-              </tbody>
-            </table>
+
+          <div id="main">
+            <div className="header">
+              <h1>CLIENTES</h1>
+            </div>
+            <div className="content" id="content">
+              <div className="pure-form pure-form-aligned">
+                <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+                  <InputCustomizado id="cpf" type="text" name="cpf" value={this.state.cpf} onChange={this.setcpf} label="CPF" />
+                  <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome" />
+                  <InputCustomizado id="telefone" type="text" name="telefone" value={this.state.telefone} onChange={this.setTelefone} label="Telefone" />
+                  <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email" />
+                  <InputCustomizado id="usuario" type="text" name="usuario" value={this.state.usuario} onChange={this.setUsuario} label="Usuário" />
+                  <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha" />
+                  <label></label>
+                  <button type="submit" className="pure-button pure-button-primary">Gravar</button>
+                </form>
+
+              </div>
+              <div>
+                <table className="pure-table">
+                  <thead>
+                    <tr>
+                      <th>CPF</th>
+                      <th>Nome</th>
+                      <th>Telefone</th>
+                      <th>Email</th>
+                      <th>Usuario</th>
+                      <th>Senha</th>
+                      <th>Ação</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                      this.state.listaCliente.map((cliente) => {
+                        return (
+                          <tr key={cliente.id}>
+                            <td>{cliente.cpf}</td>
+                            <td>{cliente.nome}</td>
+                            <td>{cliente.telefone}</td>
+                            <td>{cliente.email}</td>
+                            <td>{cliente.usuario}</td>
+                            <td>{cliente.senha}</td>
+                            <td>{cliente.acao}
+                              <button name='excluir' onClick={() => this.excluirForm(cliente.id)}><IoIosTrash/></button>
+                              <button name='editar' onClick={() => this.editaForm(cliente.id)}><IoMdBuild/></button>
+                            </td>
+                          </tr>
+                        );
+                      })
+                    }
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
     </div>
   );
-}
+  }
 }
 
 export default App;
